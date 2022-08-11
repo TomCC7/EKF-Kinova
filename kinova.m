27e1860ac6689782465f18e2f517d059f9cf2fea
cc <cctom@umich.edu>
2022-08-11 17:30:13 -0400

    update

--- kinova.m
+++ kinova.m
@@ -4,34 +4,34 @@ rob = importrobot("gen3.urdf")
 rob.DataFormat = 'col';
 rob.Gravity = [0;0;-9.81];
 n=14;      %number of state
-q=0.1;    %std of process 
-r=0.1;    %std of measurement
+q=0.1;    %std of process
+r=1;    %std of measurement
 Q=q^2*eye(n); % covariance of process
 R=r^2*eye(n);        % covariance of measurement
 
 h=@(x)[x];  % measurement equation
 
 %% Simulation
-time = 0.5;
+N = 100;
 % init state
 s = zeros(n, 1);
 x = s + q * randn(n, 1);
 torque = zeros(7, 1);
 % init state cov
-P = eye(n); % cov
-[t,sV] = ode45(@(t,y) deriv_state(y, rob, torque), [0,time], s);
-f = @(x)[next_state(x, rob, torque, k, t)];
-N = length(t);
+P = zeros(n); % cov
+% [t,sV] = ode45(@(t,y) deriv_state(y, rob, torque), [0,time], s);
 % state estimation
 xV = zeros(n, N);
 % state actual
 xV = zeros(n, N);
 % measurement
 zV = zeros(n, N);
-sV = sV';
+t = linspace(0,1, N);
+sV = zeros(n, N);
 for k=2:N
+    f = @(x)[next_state(x, rob, torque, k, t)];
     sV(:, k) = f(sV(:, k-1)) + q * randn(n, 1);
-    disp(sV(:,k))
+    % disp(sV(:,k))
     z = h(sV(:, k)) + r * randn(n, 1); % measure
     zV(:, k) = z;
     [x, P] = ekf(f, x, P, h, z, Q, R);
